9852934c69bde2a70b26076023dc8b556bd1b9e6
Nur988 <46999128+Nur988@users.noreply.github.com>
2020-03-23 16:04:10 +0600

    ui update
    made the ui update with curves
    cbut curves cilor shading and  requires modification

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+
+Color colorOne=Colors.deepPurple[170];
+Color colorTwo=Colors.deepPurple[200];
+Color colorThree=Colors.deepPurple[300];
+
+final appTheme=ThemeData(primarySwatch:Colors.purple,);

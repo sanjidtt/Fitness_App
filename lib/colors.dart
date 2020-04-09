0ca713daa4cb4c91b2b383609c0e82ca5b04115f
sanjidtt <sanjidtausif@gmail.com>
2020-04-09 10:51:08 +0600

    empty branch

--- lib/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-import 'package:flutter/material.dart';
-
-
-Color colorOne=Colors.deepPurple[170];
-Color colorTwo=Colors.deepPurple[200];
-Color colorThree=Colors.deepPurple[300];
-
-final appTheme=ThemeData(primarySwatch:Colors.purple,);
